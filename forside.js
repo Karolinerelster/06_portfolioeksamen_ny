94fd0d0d2e01c84d0fd321840879e91db66c62e2
karolinerelster <karo2739@stud.kea.dk>
2021-01-11 22:09:40 +0100

    rettelser css

--- forside.js
+++ forside.js
@@ -1,7 +1,17 @@
-document.querySelector("#box_sprite2").addEventListener("mouseover", hopRed);
+document.querySelector("#box_sprite").addEventListener("mouseover", hopRed);
+
+
+document.querySelector("#box_sprite2").addEventListener("stop", shake);
 
 function hopRed() {
     console.log("hop")
+    document.querySelector("#box_container2").classList.toggle("hop")
+
+
+}
+
+function hopRed() {
+    console.log("shake")
     document.querySelector("#box_container2").classList.toggle("hop")
 
 
